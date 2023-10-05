836e2c5f58bf7c080cd69a4b1aceb0716ecf03d2
motajessica <motajessicaa@gmail.com>
2023-10-06 10:20:25 +1300

    WIP - Fix theme toggle

--- public/themeToggle.js
+++ public/themeToggle.js
@@ -1,10 +1,14 @@
 export function toggleTheme() {
-    const currentTheme = document.body.getAttribute('data-theme');
-    
-    if (currentTheme === 'light' || !currentTheme) { // The !currentTheme is to handle if the attribute isn't set initially
-      document.body.setAttribute('data-theme', 'dark');
-    } else {
-      document.body.setAttribute('data-theme', 'light');
-    }
+  const currentTheme = document.body.getAttribute('data-theme');
+
+  if (currentTheme === 'light' || !currentTheme) {
+     document.body.setAttribute('data-theme', 'dark');
+     document.querySelector(".toggle-container .fa-moon").classList.replace("fa-moon", "fa-sun");
+  } else {
+     document.body.setAttribute('data-theme', 'light');
+     document.querySelector(".toggle-container .fa-sun").classList.replace("fa-sun", "fa-moon");
   }
-  
+}
+
+// Attach the event listener
+document.getElementById("theme-toggle").addEventListener("change", toggleTheme);
